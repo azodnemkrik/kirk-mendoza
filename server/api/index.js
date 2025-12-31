569330c1a7561d885694ebc65161a81383f5150c
Kirk <kirk!>
2025-12-31 10:31:32 -0600

    Vercel deployment fixes, removed old /api directory.

--- server/api/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const app = express.Router()
-
-//define api routes here
-app.use('/users', require('./users'))
-app.use('/reach-out', require('./reach-out'))
-
-module.exports = app
